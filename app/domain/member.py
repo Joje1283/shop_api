6a4e53a639195581ba69896a22c8e6a9b627aa6b
Jaesik <joje1283@gmail.com>
2022-04-16 21:54:20 +0900

    Member 엔티티 수정

--- app/domain/member.py
+++ app/domain/member.py
@@ -1,15 +1,15 @@
+from typing import Iterable
+
 from .address import Address
+from .orders import Order
 
 
 class Member:
-    id: str
-    name: str
-    address: Address
-
-    def __init__(self, id: str, name: str, address: Address):
+    def __init__(self, id: str, name: str, address: Address, orders: Iterable[Order]):
         self.id: str = id
         self.name: str = name
         self.address: Address = address
+        self.orders = orders
 
     def __eq__(self, o: object) -> bool:
         if isinstance(o, Member):
