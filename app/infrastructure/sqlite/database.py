a4922b9943dc0e0b43f9ace7f029c64443b9f451
Jaesik <joje1283@gmail.com>
2022-04-16 16:22:56 +0900

    인프라 계층 디렉토리 구조 생성

--- app/infrastructure/sqlite/database.py
+++ app/infrastructure/sqlite/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./db/sqlite.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -11,3 +10,7 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
