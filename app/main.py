48e7d6fb3c32aa9b0510578fc47fa87b352d4c95
Jaesik <joje1283@gmail.com>
2022-05-08 21:51:45 +0900

    main함수에 database코드 추가

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 
+from .database import SessionLocal, engine, Base
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
 async def root():
-    return {"message": "배포 테스트22"}
+    return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
