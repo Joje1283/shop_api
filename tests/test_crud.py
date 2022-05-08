d286b870778a2e33e094f3a3845e763b1be30e1c
Jaesik <joje1283@gmail.com>
2022-05-08 21:34:48 +0900

    pytest 적용 및 배포 테스트

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,10 +1,8 @@
 from sqlalchemy.orm import Session
-
-from app.crud import Member
-from app.schemas import MemberCreate
-from tests.database import test_session, reset_test_db
+from tests.database import test_session
 
 
 class TestMember:
     def test_테스트(self, db: Session = test_session):
         pass
+
