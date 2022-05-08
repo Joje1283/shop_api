959d7136f25acca2449830ae6587adf5d5d10e9c
Jaesik <joje1283@gmail.com>
2022-05-08 20:18:35 +0900

    점진적 설계를 위해 기존 코드 삭제

--- app/domain/address.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Address:
-    city: str
-    street: str
-    zipcode: str
--- app/domain/entity.py
+++
@@ -1,5 +0,0 @@
-class Entity:
-    def __eq__(self, o: object) -> bool:
-        if isinstance(o, self.__class__):
-            return self.id == o.id
-        return False
